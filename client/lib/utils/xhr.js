d807270966c5d58f6544614589da27571eade23b
ryujinzz <kdo2240k@naver.com>
2023-12-21 11:34:24 +0900

    서버연결 직전까지

--- client/lib/utils/xhr.js
+++ client/lib/utils/xhr.js
@@ -9,33 +9,179 @@
 4: complete
 
 */
-function xhr({ method, url, onsuccess, onFail }) {
+function xhr({
+  method = "GET",
+  url = "",
+  onSuccess = null,
+  onFail = null,
+  body = null,
+  headers = {
+    //사용설명서 수정
+    "Content-Type": "application/json",
+  },
+}) {
   const xhr = new XMLHttpRequest();
 
   xhr.open(method, url);
 
+  //사용설명서를 수정하려면 사용설명서 자체가 미와 벨류로 받아오기 때문에 이렇게 설정을 해야한다.
+  Object.entries(headers).forEach(([key, value]) => {
+    xhr.setRequestHeader(key, value);
+  });
+
   xhr.addEventListener("readystatechange", () => {
-    //
-    const { readyState, status, response } = xhr;
+    const { readyState, status, response } = xhr; // 객체의 구조분해할당
 
     if (readyState === 4) {
-      console.log("통신완료");
       if (status >= 200 && status < 400) {
-        console.log("통신성공");
-        //string인 것을 다시 데이터 JSON으로 변환시켜줌
-        return JSON.parse(response);
+        onSuccess(JSON.parse(response));
       } else {
-        console.log("통신 실패");
+        onFail({ message: "에러가 발생했습니다." });
       }
     }
   });
-
-  xhr.send();
+  xhr.send(JSON.stringify(body));
 }
 
-xhr({
+// xhr({
+//   method: "POST",
+//   url: "https://jsonplaceholder.typicode.com/users",
+//   onSuccess(data) {
+//     console.log(data);
+//   },
+//   onFail({ message }) {
+//     console.log(message);
+//   },
+//   body: { name: "tiger" },
+// });
+
+xhr.get = (url, onSuccess, onFail) => {
+  xhr({
+    url,
+    onSuccess,
+    onFail,
+  });
+};
+
+xhr.post = (url, body, onSuccess, onFail) => {
+  xhr({
+    method: "POST",
+    url,
+    body,
+    onSuccess,
+    onFail,
+  });
+};
+
+xhr.put = (url, body, onSuccess, onFail) => {
+  xhr({
+    method: "PUT",
+    url,
+    body,
+    onSuccess,
+    onFail,
+  });
+};
+
+xhr.delete = (url, onSuccess, onFail) => {
+  xhr({
+    method: "DELETE",
+    url,
+    onSuccess,
+    onFail,
+  });
+};
+
+console.dir(xhr);
+
+// xhr.get(
+//   "https://jsonplaceholder.typicode.com/users",
+//   (data) => {
+//     console.log(data);
+//   },
+//   (err) => {
+//     console.log(err);
+//   }
+// );
+
+//쉬프트 옵션 엑스
+/* -------------------------------------------------------------------------- */
+/*                               XHR promise API                              */
+/* -------------------------------------------------------------------------- */
+
+const defaultOptions = {
   method: "GET",
-  url: "https://jsonplaceholder.typicode.com/users",
-  onsuccess() {},
-  onFail() {},
-});
+  url: "",
+  body: null,
+  errorMessage: "서버와의 통신이 원활하지 않습니다.",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
+};
+
+export function xhrPromise(options) {
+  const { method, url, body, errorMessage, headers } = {
+    ...defaultOptions,
+    ...options,
+    headers: {
+      ...defaultOptions.headers,
+      ...options.headers,
+    },
+  };
+  const xhr = new XMLHttpRequest();
+
+  xhr.open(method, url);
+
+  Object.entries(headers).forEach(([key, value]) => {
+    xhr.setRequestHeader(key, value);
+  });
+
+  xhr.send(JSON.stringify(body));
+
+  return new Promise((resolve, reject) => {
+    xhr.addEventListener("readystatechange", () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status >= 200 && xhr.status < 400) {
+          resolve(JSON.parse(xhr.response));
+        } else {
+          reject({ message: errorMessage });
+        }
+      }
+    });
+  });
+}
+
+// xhrPromise({
+//   url:'https://jsonplaceholder.typicode.com/users'
+// })
+// .then((res)=>{
+//   console.log( res );
+// })
+
+xhrPromise.get = (url) => {
+  return xhrPromise({ url });
+};
+
+xhrPromise.post = (url, body) => {
+  return xhrPromise({
+    url,
+    body,
+    method: "POST",
+  });
+};
+
+xhrPromise.put = (url, body) => {
+  return xhrPromise({
+    url,
+    body,
+    method: "PUT",
+  });
+};
+
+xhrPromise.delete = (url) => {
+  return xhrPromise({
+    url,
+    method: "DELETE",
+  });
+};
--- client/main.js
+++ client/main.js
@@ -1,1 +1,68 @@
-import {} from "./lib/index.js";
+/* global gsap*/
+import {
+  insertLast,
+  xhrPromise,
+  tiger,
+  getNode as $,
+  renderUserCard,
+  changeColor,
+  renderSpinner,
+  delayP,
+  renderEmptyCard,
+} from "./lib/index.js";
+// xhrPromise.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+//   res.forEach((item) => {
+//     insertLast(document.body, `<div>${item.name}</div>`);
+//   });
+// });
+
+const END_POINT = "https://jsonplaceholder.typicode.com/users";
+//phase - 1
+
+const userCardInner = $(".user-card-inner");
+//1. user 데이터를 fetch하기
+//2. 함수 안에 넣기
+async function renderUserList() {
+  renderSpinner(userCardInner);
+  try {
+    await delayP(2000);
+    gsap.to(".loadingSpinner", {
+      opacity: 0,
+      onComplete() {
+        $(".loadingSpinner").remove();
+      },
+    });
+    const response = await tiger.get(END_POINT);
+    const userData = response.data;
+    userData.forEach(
+      (data) => renderUserCard(userCardInner, data)
+      //3. 유저 테이터 (이름만) 화면에 렌더링
+    );
+    changeColor(".user-card");
+    gsap.from(".user-card", {
+      x: 100,
+      opacity: 0,
+      stagger: {
+        from: "edge",
+        each: 0.1,
+      },
+    });
+  } catch (err) {
+    renderEmptyCard(userCardInner);
+  }
+}
+
+renderUserList();
+
+function handleDelete(e) {
+  //이벤트 위임
+  const button = e.target.closest("button");
+  //버튼만 수집
+  const article = e.target.closest("article");
+  //아티클만 수집
+  if (!article || !button) return;
+  const id = article.dataset.index.slice(5);
+  tiger.delete(`${END_POINT}/${id}`);
+}
+
+userCardInner.addEventListener("click", handleDelete);
